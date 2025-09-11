8ba1d39364d4bc09a8707be1d76331ae83f39367
JuanMumbach <Juanimumbach@gmail.com>
2025-09-11 12:38:36 -0300

    Secrets setup

--- Program.cs
+++ Program.cs
@@ -1,3 +1,6 @@
+using Microsoft.Extensions.Configuration;
+using VentasApp.Views;
+
 namespace VentasApp
 {
     internal static class Program
@@ -11,7 +14,14 @@ namespace VentasApp
             // To customize application configuration such as set high DPI settings or default font,
             // see https://aka.ms/applicationconfiguration.
             ApplicationConfiguration.Initialize();
-            Application.Run(new Form1());
+
+            IConfiguration configuration = new ConfigurationBuilder()
+                .AddUserSecrets<MainView>()
+                .Build();
+
+            string connectionString = configuration["MySqlConnection"];
+
+            Application.Run(new MainView());
         }
     }
 }
