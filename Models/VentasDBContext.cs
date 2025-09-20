e011734e46bd60813bc9c98a18d11e6490ea570c
JuaniM <jmb.videogames@gmail.com>
2025-09-20 16:49:35 -0300

    EF Setup

---
+++ Models/VentasDBContext.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VentasApp.Models
+{
+    public class VentasDBContext : DbContext
+    {
+        public DbSet<ProductModel> Products { get; set; }
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            IConfiguration configuration = new ConfigurationBuilder()
+                .AddUserSecrets<VentasDBContext>()
+                .Build();
+
+            string connectionString = configuration["MySqlConnection"];
+
+            optionsBuilder.UseMySql(connectionString, ServerVersion.AutoDetect(connectionString));
+        }
+    }
+}
