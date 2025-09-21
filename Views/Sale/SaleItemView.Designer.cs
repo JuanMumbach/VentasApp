26beab715347d5b7fbf34f0922a551bfcd1c9710
JuanMumbach <juanimumbach@gmail.com>
2025-09-21 18:14:26 -0300

    SaleView and SaleItemView design

---
+++ Views/Sale/SaleItemView.Designer.cs
@@ -0,0 +1,130 @@
+﻿namespace VentasApp.Views.Sale
+{
+    partial class SaleItemView
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            label1 = new Label();
+            SearchTextbox = new TextBox();
+            dataGridView1 = new DataGridView();
+            label2 = new Label();
+            AmountTextbox = new TextBox();
+            AddButton = new Button();
+            CancelButton = new Button();
+            ((System.ComponentModel.ISupportInitialize)dataGridView1).BeginInit();
+            SuspendLayout();
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Location = new Point(21, 24);
+            label1.Name = "label1";
+            label1.Size = new Size(232, 20);
+            label1.TabIndex = 0;
+            label1.Text = "Buscar producto por nombre o ID";
+            // 
+            // SearchTextbox
+            // 
+            SearchTextbox.Location = new Point(259, 21);
+            SearchTextbox.Name = "SearchTextbox";
+            SearchTextbox.Size = new Size(453, 27);
+            SearchTextbox.TabIndex = 1;
+            // 
+            // dataGridView1
+            // 
+            dataGridView1.Anchor = AnchorStyles.Top | AnchorStyles.Bottom | AnchorStyles.Left | AnchorStyles.Right;
+            dataGridView1.ColumnHeadersHeightSizeMode = DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            dataGridView1.Location = new Point(21, 64);
+            dataGridView1.Name = "dataGridView1";
+            dataGridView1.RowHeadersWidth = 51;
+            dataGridView1.Size = new Size(767, 220);
+            dataGridView1.TabIndex = 2;
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Location = new Point(21, 306);
+            label2.Name = "label2";
+            label2.Size = new Size(69, 20);
+            label2.TabIndex = 3;
+            label2.Text = "Cantidad";
+            // 
+            // AmountTextbox
+            // 
+            AmountTextbox.Location = new Point(96, 303);
+            AmountTextbox.Name = "AmountTextbox";
+            AmountTextbox.Size = new Size(100, 27);
+            AmountTextbox.TabIndex = 4;
+            // 
+            // AddButton
+            // 
+            AddButton.Location = new Point(21, 409);
+            AddButton.Name = "AddButton";
+            AddButton.Size = new Size(94, 29);
+            AddButton.TabIndex = 5;
+            AddButton.Text = "Add item";
+            AddButton.UseVisualStyleBackColor = true;
+            // 
+            // CancelButton
+            // 
+            CancelButton.Location = new Point(136, 409);
+            CancelButton.Name = "CancelButton";
+            CancelButton.Size = new Size(94, 29);
+            CancelButton.TabIndex = 6;
+            CancelButton.Text = "Cancel";
+            CancelButton.UseVisualStyleBackColor = true;
+            // 
+            // SaleItemView
+            // 
+            AutoScaleDimensions = new SizeF(8F, 20F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(800, 450);
+            Controls.Add(CancelButton);
+            Controls.Add(AddButton);
+            Controls.Add(AmountTextbox);
+            Controls.Add(label2);
+            Controls.Add(dataGridView1);
+            Controls.Add(SearchTextbox);
+            Controls.Add(label1);
+            Name = "SaleItemView";
+            Text = "SaleItemView";
+            ((System.ComponentModel.ISupportInitialize)dataGridView1).EndInit();
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private Label label1;
+        private TextBox SearchTextbox;
+        private DataGridView dataGridView1;
+        private Label label2;
+        private TextBox AmountTextbox;
+        private Button AddButton;
+        private Button CancelButton;
+    }
+}
---
+++ Views/Sale/SaleView.Designer.cs
@@ -0,0 +1,129 @@
+﻿namespace VentasApp.Views.Sale
+{
+    partial class SaleView
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            ViewItemsPanel = new Panel();
+            SaleItemsDatagridview = new DataGridView();
+            RemoveItemButton = new Button();
+            ConfirmSaleButton = new Button();
+            EditSaleItemButton = new Button();
+            AddSaleItemButton = new Button();
+            ViewItemsPanel.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)SaleItemsDatagridview).BeginInit();
+            SuspendLayout();
+            // 
+            // ViewItemsPanel
+            // 
+            ViewItemsPanel.Anchor = AnchorStyles.Top | AnchorStyles.Bottom | AnchorStyles.Left;
+            ViewItemsPanel.BackColor = SystemColors.ControlLightLight;
+            ViewItemsPanel.Controls.Add(AddSaleItemButton);
+            ViewItemsPanel.Controls.Add(EditSaleItemButton);
+            ViewItemsPanel.Controls.Add(ConfirmSaleButton);
+            ViewItemsPanel.Controls.Add(RemoveItemButton);
+            ViewItemsPanel.Controls.Add(SaleItemsDatagridview);
+            ViewItemsPanel.Location = new Point(1, 1);
+            ViewItemsPanel.Name = "ViewItemsPanel";
+            ViewItemsPanel.Size = new Size(966, 518);
+            ViewItemsPanel.TabIndex = 1;
+            // 
+            // SaleItemsDatagridview
+            // 
+            SaleItemsDatagridview.AllowUserToAddRows = false;
+            SaleItemsDatagridview.AllowUserToDeleteRows = false;
+            SaleItemsDatagridview.AllowUserToOrderColumns = true;
+            SaleItemsDatagridview.Anchor = AnchorStyles.Top | AnchorStyles.Bottom | AnchorStyles.Left | AnchorStyles.Right;
+            SaleItemsDatagridview.ColumnHeadersHeightSizeMode = DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            SaleItemsDatagridview.Location = new Point(11, 65);
+            SaleItemsDatagridview.Name = "SaleItemsDatagridview";
+            SaleItemsDatagridview.ReadOnly = true;
+            SaleItemsDatagridview.RowHeadersWidth = 51;
+            SaleItemsDatagridview.Size = new Size(941, 351);
+            SaleItemsDatagridview.TabIndex = 0;
+            // 
+            // RemoveItemButton
+            // 
+            RemoveItemButton.Anchor = AnchorStyles.Bottom | AnchorStyles.Right;
+            RemoveItemButton.Location = new Point(832, 422);
+            RemoveItemButton.Name = "RemoveItemButton";
+            RemoveItemButton.Size = new Size(120, 29);
+            RemoveItemButton.TabIndex = 1;
+            RemoveItemButton.Text = "Eliminar Item";
+            RemoveItemButton.UseVisualStyleBackColor = true;
+            // 
+            // ConfirmSaleButton
+            // 
+            ConfirmSaleButton.Location = new Point(11, 474);
+            ConfirmSaleButton.Name = "ConfirmSaleButton";
+            ConfirmSaleButton.Size = new Size(180, 29);
+            ConfirmSaleButton.TabIndex = 2;
+            ConfirmSaleButton.Text = "Finalizar Venta";
+            ConfirmSaleButton.UseVisualStyleBackColor = true;
+            // 
+            // EditSaleItemButton
+            // 
+            EditSaleItemButton.Anchor = AnchorStyles.Bottom | AnchorStyles.Right;
+            EditSaleItemButton.Location = new Point(706, 422);
+            EditSaleItemButton.Name = "EditSaleItemButton";
+            EditSaleItemButton.Size = new Size(120, 29);
+            EditSaleItemButton.TabIndex = 3;
+            EditSaleItemButton.Text = "Editar Item";
+            EditSaleItemButton.UseVisualStyleBackColor = true;
+            // 
+            // AddSaleItemButton
+            // 
+            AddSaleItemButton.Anchor = AnchorStyles.Bottom | AnchorStyles.Right;
+            AddSaleItemButton.Location = new Point(580, 422);
+            AddSaleItemButton.Name = "AddSaleItemButton";
+            AddSaleItemButton.Size = new Size(120, 29);
+            AddSaleItemButton.TabIndex = 4;
+            AddSaleItemButton.Text = "Agregar";
+            AddSaleItemButton.UseVisualStyleBackColor = true;
+            // 
+            // SaleView
+            // 
+            AutoScaleDimensions = new SizeF(8F, 20F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(969, 516);
+            Controls.Add(ViewItemsPanel);
+            Name = "SaleView";
+            Text = "SaleView";
+            ViewItemsPanel.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)SaleItemsDatagridview).EndInit();
+            ResumeLayout(false);
+        }
+
+        #endregion
+        private Panel ViewItemsPanel;
+        private DataGridView SaleItemsDatagridview;
+        private Button ConfirmSaleButton;
+        private Button RemoveItemButton;
+        private Button AddSaleItemButton;
+        private Button EditSaleItemButton;
+    }
+}
