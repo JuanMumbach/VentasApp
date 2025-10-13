b947da5992ef564ac9c76884b55a55b0b00485c1
JuanMumbach <Juanimumbach@gmail.com>
2025-10-13 02:16:09 -0300

    CustomerListView events listeners

--- Views/Customer/CustomerListView.cs
+++ Views/Customer/CustomerListView.cs
@@ -7,14 +7,101 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement.Window;
 
 namespace VentasApp.Views.Customer
 {
-    public partial class CustomerListView : Form
+    public interface ICustomerListView
     {
+        event EventHandler AddCustomerEvent;
+        event EventHandler EditCustomerEvent;
+        event EventHandler DeleteCustomerEvent;
+        event EventHandler RestoreDeletedCustomerEvent;
+        event EventHandler ShowDeletedChangeEvent;
+        event EventHandler SearchCustomerEvent;
+
+        string searchValue { get; set; }
+        public int? GetSelectedCustomerId();
+        void SetCustomerListBindingSource(BindingSource source);
+    }
+
+    public partial class CustomerListView : Form, ICustomerListView
+    {
+        public event EventHandler AddCustomerEvent;
+        public event EventHandler EditCustomerEvent;
+        public event EventHandler DeleteCustomerEvent;
+        public event EventHandler RestoreDeletedCustomerEvent;
+        public event EventHandler ShowDeletedChangeEvent;
+        public event EventHandler SearchCustomerEvent;
         public CustomerListView()
         {
             InitializeComponent();
+            SetupEventsHandler();
+        }
+
+        public string searchValue
+        {
+            get { return SearchTextbox.Text; }
+            set { SearchTextbox.Text = value; }
+        }
+
+        private void SetupEventsHandler()
+        {
+            SearchTextbox.KeyDown += (s, e) =>
+            {
+                if (e.KeyCode == Keys.Enter)
+                    SearchCustomerEvent?.Invoke(this, EventArgs.Empty);
+            };
+
+            dataGridView1.SelectionChanged += (s, e) =>
+            {
+                UpdateDeleteButtonState();
+                UpdateRestoreButtonState();
+                UpdateEditButtonState();
+            };
+
+            ShowDeletedCheckbox.CheckedChanged += delegate { ShowDeletedChangeEvent?.Invoke(this, EventArgs.Empty); };
+
+            dataGridView1.Click += delegate { SearchCustomerEvent?.Invoke(this, EventArgs.Empty); };
+
+            AddCustomerButton.Click += delegate { AddCustomerEvent?.Invoke(this, EventArgs.Empty); };
+            EditCustomerButton.Click += delegate { EditCustomerEvent?.Invoke(this, EventArgs.Empty); };
+            DeleteButton.Click += delegate { DeleteCustomerEvent?.Invoke(this, EventArgs.Empty); };
+            RestoreDeletedButton.Click += delegate { RestoreDeletedCustomerEvent?.Invoke(this, EventArgs.Empty); };
+        }
+
+        private void UpdateDeleteButtonState()
+        {
+            var selectedProduct = GetSelectedCustomerId();
+            var isActive = (bool)dataGridView1.CurrentRow.Cells["Active"].Value;
+            DeleteButton.Enabled = selectedProduct.HasValue && isActive;
+        }
+
+        private void UpdateRestoreButtonState()
+        {
+            var selectedProduct = GetSelectedCustomerId();
+            var isActive = (bool)dataGridView1.CurrentRow.Cells["Active"].Value;
+            RestoreDeletedButton.Enabled = selectedProduct.HasValue && !isActive;
+        }
+
+        private void UpdateEditButtonState()
+        {
+            EditCustomerButton.Enabled = GetSelectedCustomerId().HasValue;
+        }
+
+        public int? GetSelectedCustomerId()
+        {
+            if (dataGridView1.CurrentRow != null)
+            {
+                // Assuming the first column is the Product's ID
+                return (int)dataGridView1.CurrentRow.Cells["Id"].Value;
+            }
+            return null;
+        }
+
+        public void SetCustomerListBindingSource(BindingSource source)
+        {
+            dataGridView1.DataSource = source;
         }
     }
 }
