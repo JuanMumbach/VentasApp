00a0dd8add4f12e63c3d260ed02598aed8c3957d
JuanMumbach <Juanimumbach@gmail.com>
2025-11-09 15:32:15 -0300

    Dashboard view creation

---
+++ Views/Dashboard/DashboardView.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace VentasApp.Views.Dashboard
+{
+    public interface IDashboardView : IBaseForm
+    {
+
+    }
+
+    public partial class DashboardView : BaseForm, IDashboardView
+    {
+        public DashboardView()
+        {
+            InitializeComponent();
+        }
+    }
+}
